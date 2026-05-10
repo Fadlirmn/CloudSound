634306c15f530fd66c4830cc62daf39e910b34cb
sumbul <haniffadlir@gmail.com>
2026-05-10 20:48:59 +0700

    update logic url backend

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,7 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  // Load env file based on `mode` in the current working directory.
-  // Set the third parameter to '' to load all envs regardless of the `VITE_` prefix.
-  const env = loadEnv(mode, process.cwd(), '');
-  
-  return {
-    plugins: [react()],
-    server: {
-      proxy: {
-        '/api': {
-          target: env.VITE_PROXY_TARGET || 'http://localhost:8080',
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, ''),
-        },
-      },
-    },
-  }
+export default defineConfig({
+  plugins: [react()],
 })
